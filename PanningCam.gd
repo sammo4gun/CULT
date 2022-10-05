e47bc8948843f1daff3e52fd2ed22ae6e88a97c2
sammo4gun <33280495+sammo4gun@users.noreply.github.com>
2022-10-05 15:38:46 +0200

    first commit

---
+++ PanningCam.gd
@@ -0,0 +1,36 @@
+extends Camera2D
+
+const MIN_ZOOM: float = 0.1
+const MAX_ZOOM: float = 1.0
+const ZOOM_INCREMENT: float = 0.1
+
+const ZOOM_RATE: float = 8.0
+
+var _target_zoom: float = 1.0
+
+func _ready():
+	pass
+
+func _physics_process(delta) -> void:
+	zoom = lerp(zoom, _target_zoom * Vector2.ONE, ZOOM_RATE * delta)
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		if event.button_mask == BUTTON_MASK_MIDDLE:
+			position -= event.relative * zoom
+	
+	if event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				zoom_in()
+			if event.button_index == BUTTON_WHEEL_DOWN:
+				zoom_out()
+
+func zoom_in():
+	_target_zoom = max(_target_zoom - ZOOM_INCREMENT, MIN_ZOOM)
+	set_physics_process(true)
+	
+func zoom_out():
+	_target_zoom = min(_target_zoom + ZOOM_INCREMENT, MAX_ZOOM)
+	set_physics_process(true)
+	
